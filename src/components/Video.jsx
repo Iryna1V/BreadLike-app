21273e4e4e7a45faeaa9fe25894101bb10eaebb9
Iryna1V <ivhaievska@gmail.com>
2026-01-21 11:05:53 -0500

    changes

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -3,11 +3,11 @@ import video from "../assets/video.mp4";
 
 function Video() {
   return (
-    <>
+    <div>
       <video autoPlay muted loop>
         <source src={video} type="video/mp4" />
       </video>
-    </>
+    </div>
   );
 }
 
