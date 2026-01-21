a7d252d7dc67d01bb146bd20c26b441df600d116
Iryna1V <ivhaievska@gmail.com>
2026-01-21 09:45:23 -0500

    changes

--- src/data/Button.data.jsx
+++
@@ -1,6 +0,0 @@
-export const BUTTONS = [
-  { key: "white", label: "white flour" },
-  { key: "rye", label: "rye flour" },
-  { key: "wholemeal", label: "wholemeal flour" },
-  { key: null, label: "all types" },
-];
--- src/data/Counter.data.jsx
+++
@@ -1,6 +0,0 @@
-export const statsData = [
-  { id: 1, endValue: 55, label: "% Customers buy baguettes" },
-  { id: 2, endValue: 7, label: "Years baking experience" },
-  { id: 3, endValue: 75, label: "Millions bread sold" },
-  { id: 4, endValue: 15, label: "Thousand happy clients" },
-];
